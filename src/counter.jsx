72a6472235f174179984a50e5fc59f8b0817f8d2
safety-panda <earmiffs+git@googlemail.com>
2023-10-05 11:43:38 +0100

    added bootstap classes to counter components

--- src/counter.jsx
+++ src/counter.jsx
@@ -7,20 +7,18 @@ const Counter = () => {
   const subtract = () => setcount((prev) => prev - 1);
 
   return (
-    <div className="">
-      <h1 className="">
-        Counter Component
-      </h1>
-      <h2 className="">{count}</h2>
-      <div className="">
+    <div className="mt-5 px-3 py-4">
+      <h1 className="h1 display-1">Counter Component</h1>
+      <h2 className="h1 display-1 mt-5 mb-3">{count}</h2>
+      <div className="mt-5">
         <button
-          className=""
+          className="btn btn-primary btn-lg mx-3 px-5 py-3 mt-2"
           onClick={add}
         >
           Add
         </button>
         <button
-          className=""
+          className="btn btn-dark btn-lg mx-3 px-5 py-3 mt-2"
           onClick={subtract}
         >
           Remove
